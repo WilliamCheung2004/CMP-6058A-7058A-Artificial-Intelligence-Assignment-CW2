dd8ec6c6abc2e43f40a112257d68b3fdddf313c2
WilliamCheung2004 <WilliamCheung2004@gmail.com>
2025-12-08 18:09:44 +0000

    Added file reading/writing
    Creates csv if doesn't exist
    Else checks if labels currently exists if not adds them
    Then adds data (x,y,z) to csv file

--- mediapipe_test.py
+++ mediapipe_test.py
@@ -1,5 +1,8 @@
 import mediapipe as mp
 import cv2
+import csv
+import numpy as np
+import os 
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from mediapipe.framework.formats import landmark_pb2
@@ -8,6 +11,9 @@ BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
+
+labels = ['x','y','z']
+dataFile = "data.csv"
 
 options = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path="hand_landmarker.task"),
@@ -21,17 +27,40 @@ with HandLandmarker.create_from_options(options) as landmarker:
 
     annotated_image = image.numpy_view().copy()
     annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR)
+    
+    #Check if csv exists
+    if not os.path.exists(dataFile):
+        with open(dataFile, "w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(labels)
+            
+    #Check if labels are currently written 
+    else:
+        with open(dataFile,'r') as f:
+            reader = csv.reader(f)
+            row1 = next(reader,None)
+            
+            print(row1)
+            
+        if row1 is not None and [col.strip().lower() for col in row1] == labels:
+            print("already printed")
+             
+        else:
+            with open(dataFile,'a', newline="\n") as f:
+                writer = csv.writer(f)
+                writer.writerow(labels)
 
+    # Extract x, y, z for each landmark
     for hand_landmarks in result.hand_landmarks:
-        
-        for hand_landmarks in result.hand_landmarks:
-        
-         # Extract x, y, z for each landmark
          xyz_list = [(lm.x, lm.y, lm.z) for lm in hand_landmarks]
-         print("Landmark coordinates:")
-        for i, (x, y, z) in enumerate(xyz_list):
-            print(f"  Landmark {i}: x={x:.4f}, y={y:.4f}, z={z:.4f}")
-       
+
+
+    #Writing x,y,z data to csv   
+    with open(dataFile,'a', newline="") as f:
+        writer = csv.writer(f)
+        for (x,y,z) in xyz_list:
+            writer.writerow([x,y,z]) 
+        writer.writerow("")          
 
         landmark_list = landmark_pb2.NormalizedLandmarkList(
             landmark=[
