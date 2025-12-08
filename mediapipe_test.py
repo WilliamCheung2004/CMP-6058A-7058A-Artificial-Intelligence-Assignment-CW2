3dead058c2db3d25c7dcef0d551dca1e588bb8d7
WilliamCheung2004 <WilliamCheung2004@gmail.com>
2025-12-08 08:27:53 +0000

    Added method to get coordinate data
    Gets x y z data from hand

--- mediapipe_test.py
+++ mediapipe_test.py
@@ -23,6 +23,15 @@ with HandLandmarker.create_from_options(options) as landmarker:
     annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR)
 
     for hand_landmarks in result.hand_landmarks:
+        
+        for hand_landmarks in result.hand_landmarks:
+        
+         # Extract x, y, z for each landmark
+         xyz_list = [(lm.x, lm.y, lm.z) for lm in hand_landmarks]
+         print("Landmark coordinates:")
+        for i, (x, y, z) in enumerate(xyz_list):
+            print(f"  Landmark {i}: x={x:.4f}, y={y:.4f}, z={z:.4f}")
+       
 
         landmark_list = landmark_pb2.NormalizedLandmarkList(
             landmark=[
